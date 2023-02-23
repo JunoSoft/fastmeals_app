e830990745c5599cc26e803559bda8c45e1cbea7
Junosoft <junosoftltd@gmail.com>
2023-02-23 23:45:59 +0200

    updated

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,6 +7,6 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Screen Categoris initiation'),);
+    return GridView(gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200,childAspectRatio: 3/2,crossAxisSpacing: 20,mainAxisSpacing: 20),);
   }
 }
---
+++ lib/dummy_data.dart
@@ -0,0 +1,7 @@
+import './models/category.dart';
+const DUMMY_CATEGORIES = const [Category(id: 'C1', title: 'Italian',color: Colors.purple),
+Category(id: 'C1', title: 'Italian',color: Colors.purple),
+Category(id: 'C1', title: 'Italian',color: Colors.purple),
+Category(id: 'C1', title: 'Italian',color: Colors.purple),
+Category(id: 'C1', title: 'Italian',color: Colors.purple),
+Category(id: 'C1', title: 'Italian',color: Colors.purple),]
