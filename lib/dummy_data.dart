6e8319a7a42bca327c0aa2d7f516eb70fe44bdd8
Junosoft <junosoftltd@gmail.com>
2023-02-24 00:14:58 +0200

    added dummy category

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
 import './models/category.dart';
-const DUMMY_CATEGORIES = const [Category(id: 'C1', title: 'Italian',color: Colors.purple),
-Category(id: 'C1', title: 'Italian',color: Colors.purple),
-Category(id: 'C1', title: 'Italian',color: Colors.purple),
-Category(id: 'C1', title: 'Italian',color: Colors.purple),
-Category(id: 'C1', title: 'Italian',color: Colors.purple),
-Category(id: 'C1', title: 'Italian',color: Colors.purple),]
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'C1', title: 'Italian', color: Colors.purple),
+  Category(id: 'C1', title: 'Italian', color: Colors.purple),
+  Category(id: 'C2', title: 'Quick and Easy', color: Colors.red),
+  Category(id: 'C3', title: 'Humbergers', color: Colors.pink),
+  Category(id: 'C4', title: 'Vegies', color: Colors.green),
+  Category(id: 'C5', title: 'French', color: Colors.yellow),
+  Category(id: 'C6', title: 'Asian', color: Colors.lightGreen),
+  Category(id: 'C7', title: 'Indian', color: Colors.lightBlue),
+  Category(id: 'C8', title: 'Americano', color: Colors.teal),
+  Category(id: 'C9', title: 'German', color: Colors.blueGrey),
+  Category(id: 'C10', title: 'Summer', color: Colors.brown),
+];
+
