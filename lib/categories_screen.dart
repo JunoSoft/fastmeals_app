d5f0a27b749b4151f33681bb305dd1ba200cbac4
Junosoft <junosoftltd@gmail.com>
2023-02-24 10:09:57 +0200

    added asstes-fonts directort

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -2,11 +2,19 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'package:flutter/material.dart';
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200,childAspectRatio: 3/2,crossAxisSpacing: 20,mainAxisSpacing: 20),);
+    return GridView(
+      
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+    );
   }
 }
