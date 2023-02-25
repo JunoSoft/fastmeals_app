3cf0e6222c0e69eb7ae5039a7c4a4dd688a47171
Junosoft <junosoftltd@gmail.com>
2023-02-25 11:10:48 +0200

    added where for filtering

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
-  static const routeName ='/category_meals';
+  static const routeName = '/category_meals';
 // CategoryMealsScreen(
 //       {super.key});
 
@@ -10,12 +12,21 @@ class CategoryMealsScreen extends StatelessWidget {
     final routesArgs =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routesArgs['title'];
+    final categoryId = routesArgs['id'];
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: const Center(
-        child: Text('REcipes View'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
