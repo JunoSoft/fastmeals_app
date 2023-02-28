ca4780409edd8254070a896474ac7138e919ebbe
Junosoft <junosoftltd@gmail.com>
2023-02-28 23:41:09 +0200

    added try.dart

---
+++ lib/screens/try.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Try extends StatelessWidget {
+  const Try({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
